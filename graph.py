57699e3023e4edc7f25b5da3e21b95230c6aeafc
swp1kmu <seochoking@naver.com>
2020-06-16 11:08:45 +0900

    lecture 14-2 src prepared

--- graph.py
+++ graph.py
@@ -1,13 +1,11 @@
 from cgi import parse_qs
 from template import html
-import matplotlib as mpl
-mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 def application(environ, start_response):
-    if environ['PATH_INFO'] == '/img/graph.png':
+    if environ['PATH_INFO'] == '/graph.png':
         try:
-            with open('./img/graph.png', 'rb') as f:
+            with open('graph.png', 'rb') as f:
                 response_body = f.read()
         except:
             response_body = ''
